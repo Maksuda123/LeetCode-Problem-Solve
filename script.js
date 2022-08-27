e2b373e799e2a6896e958e8f48a0b928f2c4ce36
Maksuda123 <afraazra171@gmail.com>
2022-08-27 14:02:20 +0600

    Roman to Integer

--- script.js
+++ script.js
@@ -24,3 +24,18 @@ const palindromeNumber = (x) => {
     return res === x;
 }
 console.log(palindromeNumber(121))
+
+//13. Roman to Integer
+const romanToInteger = (romanStr) => {
+    let num = 0;
+let objRoman = {M:1000,D:500,C:100,L:50,X:10,V:5,I:1};
+for(let i=0; i<romanStr.length; i++){
+    if(objRoman[romanStr[i] < objRoman[romanStr[i+1]]]){
+        num -=objRoman[romanStr[i]];
+    }else{
+        num += objRoman[romanStr[i]];
+    }
+}
+return num;
+}
+console.log(romanToInteger('III'))

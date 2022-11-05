0013634caf3f8918fe46d5cc2d25ab1ce6c7c31c
Maksuda123 <afraazra171@gmail.com>
2022-11-05 18:18:02 +0600

    Solved Valid Parentheses problem

--- script.js
+++ script.js
@@ -53,3 +53,24 @@ const longestCommponPrefix = (strs) => {
     return strs[0];
 };
 console.log(longestCommponPrefix(["flower","flow","flight"]))
+
+
+// 20. Valid Parentheses
+const isValid = (s) => {
+    var stack = [];                                                
+    var len = s.length;
+    var map = {
+      '(': ')',
+      '[': ']',
+      '{': '}'
+    };
+    for (let i = 0; i < len; i++) {
+      if (stack.length > 0 && map[stack[stack.length - 1]] === s[i]) {
+        stack.pop();
+      } else {
+        stack.push(s[i]);
+      }
+    }
+    return stack.length === 0;
+  };
+  console.log(isValid("()[]{}"))

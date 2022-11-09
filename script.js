33e6cd6530d293267239eb86b53696dd9b159eec
Maksuda123 <afraazra171@gmail.com>
2022-11-09 15:54:23 +0600

    Merge Two Lists

--- script.js
+++ script.js
@@ -74,3 +74,26 @@ const isValid = (s) => {
     return stack.length === 0;
   };
   console.log(isValid("()[]{}"))
+
+
+
+
+  //   21. Merge Two Sorted Lists
+  var mergeTwoLists = function(l1, l2) {   
+    var head = new ListNode(0);
+    var now = head;
+    var p1 = l1;
+    var p2 = l2;
+    while (p1 || p2) {
+      if (p1 === null || (p2 !== null && p2.val < p1.val)) {
+        now.next = p2;
+        p2 = p2.next;
+      } else {
+        now.next = p1;
+        p1  = p1.next;
+      }
+      now = now.next;
+    }
+    return head.next;
+  };
+  

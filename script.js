bcbcaa594e12c8b92e8e4c0936758cddf32da76a
Maksuda123 <afraazra171@gmail.com>
2022-11-23 16:59:11 +0600

    Implement String

--- script.js
+++ script.js
@@ -132,3 +132,20 @@ var removeElement = function(nums, val) {
   }
   return count;
 };
+
+
+
+
+//28. Implement strStr()
+var strStr = function (haystack, needle) {
+  if (needle.length === 0) return 0;
+
+  if (needle === haystack) return 0;
+
+  for (let i = 0; i <= haystack.length - needle.length; i++) {
+    if (needle === haystack.substring(i, i + needle.length)) {
+      return i;
+    }
+  } 
+  return -1;
+};

1dda1ada9cb4ecfb39e6ca08b86501539ff028b4
Maksuda Mila <afraazra171@gmail.com>
2023-03-24 02:02:57 +0600

    Remove Duplicates from Sorted List

--- script.js
+++ script.js
@@ -306,3 +306,16 @@ var mySqrt = function (x) {
   }
   return mid * mid > x ? mid - 1 : mid;
 };
+
+//83. Remove Duplicates from Sorted List
+const deleteDuplicates = function (head) {
+  let curr = head;
+  while (curr && curr.next) {
+    if (curr.val === curr.next.val) {
+      curr.next = curr.next.next;
+    } else {
+      curr = curr.next;
+    }
+  }
+  return head;
+};
